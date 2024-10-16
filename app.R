e3b88a006d7c6b9ba2f85753d7a0869dda92c82c
TEDS-91 <tadeuederzootecnista@gmail.com>
2024-10-16 13:33:11 -0400

    Small addition to get_calf_requirements function.

--- app.R
+++ app.R
@@ -4,4 +4,4 @@
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options("golem.app.prod" = TRUE)
-CalfSim::run_app() # add parameters here (if any)
+CalfSim::run_app() # add parameters here
